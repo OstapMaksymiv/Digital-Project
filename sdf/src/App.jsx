4b52c02a281de6c70e5d9a67adc8d79e796c88e2
OstapMaksymiv <ostaperad@gmail.com>
2024-05-18 18:27:28 +0200

    feat:last changes

--- sdf/src/App.jsx
+++ sdf/src/App.jsx
@@ -1,11 +1,11 @@
 import Main from './pages/Main//Main'
-// import Gallery from './pages/Gallery//Gallery'
+import Gallery from './pages/Gallery//Gallery'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import {Layout} from './Layout'
-// import Projects from './pages/Projects/Projects'
-// import Project from './pages/Project/Project'
-// import Contacts from './pages/Contacts/Contacts'
-// import Certifications from './pages/Certifications/Certifications'
+import Projects from './pages/Projects/Projects'
+import Project from './pages/Project/Project'
+import Contacts from './pages/Contacts/Contacts'
+import Certifications from './pages/Certifications/Certifications'
 function App() {
 
   return (
@@ -13,11 +13,11 @@ function App() {
       <Routes>
         <Route element={<Layout/>}>
           <Route path="/" element={<Main/>}/>
-          {/* <Route path="/Gallery" element={<Gallery/>}/>
+          <Route path="/Gallery" element={<Gallery/>}/>
           <Route path="/Projects" element={<Projects/>}/>
           <Route path="/Project/:id" element={<Project/>}/>
           <Route path="/Certifications" element={<Certifications/>}/>
-          <Route path="/Contacts" element={<Contacts/>}/> */}
+          <Route path="/Contacts" element={<Contacts/>}/>
         </Route>
       </Routes>
     </Router>
